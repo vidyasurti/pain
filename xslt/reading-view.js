e708f4a10e9141c0eb9ef9f65588e362168cac18
Richa Desai <76023553+richa-d110@users.noreply.github.com>
2022-04-27 00:47:50 -0400

    animated collapsible

--- xslt/reading-view.js
+++ xslt/reading-view.js
@@ -1,12 +1,15 @@
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-var coll = document.getElementByClassName("filtering-sect");
-coll.addEventListener("click", function() {
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
-    }
-});
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
