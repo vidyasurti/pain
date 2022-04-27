d2f92319dd736dacb222672e431cc4e0ca014cd5
Richa Desai <76023553+richa-d110@users.noreply.github.com>
2022-04-27 00:27:36 -0400

    creating a javascript file for filtering

---
+++ xslt/reading-view.js
@@ -0,0 +1,12 @@
+
+
+var coll = document.getElementByClassName("filtering-sect");
+coll.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+});
